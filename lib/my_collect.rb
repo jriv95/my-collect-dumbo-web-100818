fe2ef3db8c9eba3a8986e83cbbfcab9a212d5f20
Joel Rivera <joel.rivera7995@gmail.com>
2018-10-03 03:50:41 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(collection)
   x = 0
-  new_array = []
+  new_collection = []
   while x < array.length do
-    new_array << yield(collection[x])
+    new_collection << yield(collection[x])
     x += 1
   end
-  new_array
+  new_collection
 end
