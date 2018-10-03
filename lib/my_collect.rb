9901f3738072ef80f32d3c8626c9d02d83cae5d7
Joel Rivera <joel.rivera7995@gmail.com>
2018-10-03 03:48:20 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect(collection)
   x = 0
   array = []
-  while x < array.length do |collection|
+  while x < array.length do |array|
     array << yield(collection[x])
     x += 1
   end
